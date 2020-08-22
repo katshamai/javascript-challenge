760a8f275e85154714d34fdae83482f995d71d59
Kat Shamai <64203831+katshamai@users.noreply.github.com>
2020-08-22 14:27:13 +1000

    Update app.js for appending table and rows

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Attach HTML table and add rows for UFO data
+console.log(data);
+var tbody = d3.select("tbody");
+
+data.forEach((ufoData) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
