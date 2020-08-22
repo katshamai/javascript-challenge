fae21da8c416d7b0c80af4b0c88a3a2e7aa5373c
Kat Shamai <64203831+katshamai@users.noreply.github.com>
2020-08-22 16:40:01 +1000

    creating filters

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -28,38 +28,56 @@ form.on("submit", runEnter);
 // Complete the event handler function for the form
 function runEnter() {
 
-  // // Prevent the page from refreshing
+  // Prevent the page from refreshing
   d3.event.preventDefault();
 
-  // Select the input element and get the raw HTML node
-  var inputDate = d3.select("#datetime");
-  var inputCity = d3.select("#cty");
-  var inputState = d3.select("#state");
-  var inputCountry = d3.select("#country");
-  var inputShape = d3.select("#shape");
+  // Select the input element, property, and get the raw HTML node
+  var inputDate = d3.select("#datetime").property("value");
+  var inputCity = d3.select("#city").property("value");
+  var inputState = d3.select("#state").property("value");
+  var inputCountry = d3.select("#country").property("value");
+  var inputShape = d3.select("#shape").property("value");
 
-  // Get the value property of the input element
-  var userInput = inputElement.property("value");
-  console.log(userInput);
+  console.log(inputDate,inputCity,inputState,inputCountry,inputShape);
+};
 
   // Use the form input to filter the data by date
-  var matchingResults = tableData.filter(sighting => sighting.datetime == userInput);
+  filteredData = tableData;
 
-  // Show filtered results only in main table
-  if (matchingResults.length == 0) {
-      console.log(`No results for date ${userInput}`);
-      tbody.html("");
-      tbody.text(`There are no results for the date you entered - ${userInput}`);
-    } else {
-  tbody.html("");
-  matchingResults.forEach((report) => {
-    var row = tbody.append('tr');
+  if (inputDate) {
+    filteredData = filteredData.filter(sighting => sighting.datetime == inputDate);
+  }
 
-    Object.entries(report).forEach(([key, value]) => {
-        console.log(key, value);
-        var cell = row.append('td');
-        cell.text(value);
-    });
-  });
-}; 
-};
+  if (inputCity) {
+    filteredData = filteredData.filter(sighting => sighting.city == inputCity);
+  }
+
+  if (inputState) {
+    filteredData = filteredData.filter(sighting => sighting.datetime == inputState);
+  }
+
+  if (inputCountry) {
+    filteredData = filteredData.filter(sighting => sighting.datetime == inputCountry);
+  }
+
+  if (inputShape) {
+    filteredData = filteredData.filter(sighting => sighting.datetime == inputShape);
+  }
+
+//   // Show filtered results only in main table
+//   if (matchingResults.length == 0) {
+//       console.log(`No results for date ${userInput}`);
+//       tbody.html("");
+//       tbody.text(`There are no results for the date you entered - ${userInput}`);
+//     } else {
+//   tbody.html("");
+//   matchingResults.forEach((report) => {
+//     var row = tbody.append('tr');
+
+//     Object.entries(report).forEach(([key, value]) => {
+//         console.log(key, value);
+//         var cell = row.append('td');
+//         cell.text(value);
+//     });
+//   });
+// }; 
